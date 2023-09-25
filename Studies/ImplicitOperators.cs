262ac4f2b8f282c75b597c70d238ecfd3b4c52c1
Sostenes Apollo <ctr.sostenes.dasilva@prth.com>
2023-09-25 19:41:41 -0300

    Study of Implicit Operators

---
+++ Studies/ImplicitOperators.cs
@@ -0,0 +1,50 @@
+﻿namespace ImplicitOperators;
+
+public class ImplicitOperators
+{
+    public static void Main()
+    {
+        var phone = new Phone()
+        {
+            Area = "99",
+            CountryCode = "55",
+            Number = "88284904"
+        };
+        Console.WriteLine(phone);
+
+        var number = "+55 (99) 88284904";
+        Console.WriteLine(number);
+
+    }
+}
+
+public class Phone
+{
+    public string CountryCode { get; set; }
+    public string Area { get; set; }
+    public string Number { get; set; }
+
+    public static implicit operator string(Phone phone) =>
+        $"+{phone.CountryCode} ({phone.Area}) {phone.Number}";
+
+    public static implicit operator Phone(String ph)
+    {
+        // var splited = ph.
+        var data  = ph.Split(" ");
+        return new Phone()
+        {
+            CountryCode = data[0],
+            Area = data[1],
+            Number = data[2]
+        };
+    }
+    
+    // public override string ToString()
+    // {
+    //     if (
+    //         String.IsNullOrEmpty(CountryCode)||
+    //         String.IsNullOrEmpty(Area) ||
+    //         String.IsNullOrEmpty(Number) ) return "Não definido";
+    //     return $"+{CountryCode} ({Area}) {Number}";
+    // }
+}
